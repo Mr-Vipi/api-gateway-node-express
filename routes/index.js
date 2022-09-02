fb575c97fa89529153921872fe007dcf07de3394
Jul Guga <mr.guga89@hotmail.com>
2022-09-02 23:22:05 +0200

    Add server registration endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const registry = require("../config/registry.json");
+const fs = require("fs");
 
 router.all("/:apiName/:path", (req, res) => {
   console.log("apiName:", req.params.apiName);
@@ -17,6 +18,19 @@ router.all("/:apiName/:path", (req, res) => {
   } else {
     res.send("API Name doesn't exist");
   }
+});
+
+router.post("/register", (req, res) => {
+  const registrationInfo = req.body;
+  registry.services[registrationInfo.apiName] = { ...registrationInfo };
+
+  fs.writeFile("./config/registry.json", JSON.stringify(registry), (error) => {
+    if (error) {
+      res.send(`Could not register ${registrationInfo.apiName} \n ${error}`);
+    } else {
+      res.send(`Successfully registered ${registrationInfo.apiName}`);
+    }
+  });
 });
 
 module.exports = router;
