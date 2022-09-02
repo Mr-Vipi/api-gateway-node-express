9b1779c388b251ebece773fd0b4d0e0a28709aa6
Jul Guga <mr.guga89@hotmail.com>
2022-09-02 22:17:53 +0200

    forwarding the http request from the api gateway

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -6,5 +6,8 @@ app.use(express.json());
 app.get("/fakeapi", (req, res, next) => {
   res.send("Hello from fake api server.");
 });
+app.get("/bogusapi", (req, res, next) => {
+  res.send("Bogus api says hello!");
+});
 
 app.listen(PORT, () => console.log(`Fake server started on port ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 router.all("/:apiName", (req, res) => {
   console.log("apiName:", req.params.apiName);
-  res.send(`${req.params.apiName} \n`);
+  axios.get(`http://localhost:5001/${req.params.apiName}`).then((response) => {
+    res.send(response.data);
+  });
 });
 
 module.exports = router;
