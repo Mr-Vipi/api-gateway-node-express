307e6021c0fee077efd0f903e5962d64709b4977
Jul Guga <mr.guga89@hotmail.com>
2022-09-03 00:05:59 +0200

    Api registration

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 5001;
+const axios = require("axios");
+const HOST = "http://localhost";
+const PORT = 5002;
 
 app.use(express.json());
 app.get("/fakeapi", (req, res, next) => {
@@ -10,4 +12,19 @@ app.post("/bogusapi", (req, res, next) => {
   res.send("Bogus api says hello!");
 });
 
-app.listen(PORT, () => console.log(`Fake server started on port ${PORT}`));
+app.listen(PORT, () => {
+  axios({
+    method: "POST",
+    url: "http://localhost:5000/register",
+    headers: { "Content-Type": "application/json" },
+    data: {
+      apiName: "registrytest",
+      host: HOST,
+      port: PORT,
+      url: `${HOST}:${PORT}/`,
+    },
+  }).then((response) => {
+    console.log(response.data);
+  });
+  console.log(`Fake server started on port ${PORT}`);
+});
