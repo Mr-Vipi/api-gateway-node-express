9703aebd39a75aceb16b478189aa7167a77cc8de
Jul Guga <mr.guga89@hotmail.com>
2022-09-02 21:39:44 +0200

    Init api gateway with express js

---
+++ gateway.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const PORT = 5000;
+
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(PORT, () => console.log(`Gateway started on port ${PORT}`));
